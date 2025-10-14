78da3c6f39853e34ffb7c65b2adb48001b346843
cibi-sudo <cibiccf437@gmail.com>
2025-10-14 17:21:54 +0530

    first commit

---
+++ packages/db/src/dal/user.ts
@@ -0,0 +1,28 @@
+import {prisma} from "../client.js";
+import {UserSignupInput} from "@repo/validation";
+
+export const userDAL = {
+    async createUser (data: UserSignupInput) {
+        return prisma.user.create({ data });
+    },
+
+    async findByEmail (data: Pick<UserSignupInput, "email">) {
+        return prisma.user.findUnique(
+        { where: { email: data.email } },
+        );
+    },
+
+    async updateUser (id: string, data: Partial<UserSignupInput>) {
+        return prisma.user.update({
+            where: { id },
+            data,
+        });
+    },
+
+    async deleteUser (id: string){
+        return prisma.user.delete({
+            where: { id },
+        });
+    },
+}
+
